322caf9d84947dfb032c462234c8cb4083952e8e
SunilVerma0358 <sunilverma93060@gmail.com>
2023-09-30 10:26:10 +0530

    add hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,3 +35,19 @@ finish.forEach(e => {
         nav.classList.toggle("left-corner")
     })
 });
+// back to top
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
